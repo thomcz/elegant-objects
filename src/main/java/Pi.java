945db5402dbce601f515fe404b6129e8a5a90d00
thomcz <thomas.czogalik@gmx.de>
2022-01-31 18:39:03 +0100

    add pi constant

---
+++ src/main/java/Pi.java
@@ -0,0 +1,8 @@
+public class Pi implements Constant {
+    private static final double PI = 3.14159265358979323846;
+
+    @Override
+    public double value() {
+        return PI;
+    }
+}
---
+++ src/test/java/PITest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PITest {
+    @Test
+    void piValue() {
+        Pi pi = new Pi();
+        assertThat(pi.value()).isEqualTo(3.14159265358979323846);
+    }
+}
